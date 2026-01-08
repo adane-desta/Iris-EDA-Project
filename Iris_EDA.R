638dc8ae07520bd3d71e57be5c5b5694a9ae361f
adane-desta <addonegn@gmail.com>
2026-01-08 19:43:49 +0300

    data visualization

--- Iris_EDA.R
+++ Iris_EDA.R
@@ -8,3 +8,14 @@ dim(iris)
 colSums(is.na(iris))
 summary(iris[, 1:4])
 sapply(iris[, 1:4], sd)
+
+plot(
+  iris$Sepal.Length,
+  iris$Sepal.Width,
+  col = iris$Species,
+  pch = 19,
+  xlab = "Sepal Length",
+  ylab = "Sepal Width",
+  main = "Sepal Length vs Sepal Width"
+)
+legend("topright", legend = levels(iris$Species), col = 1:3, pch = 19)
